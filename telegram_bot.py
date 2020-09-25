67118924685976c9a287632ac3c0bc942ac46bd8
makaraction <makaraction@gmail.com>
2020-09-25 19:20:25 +0900

    Aiogram

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,15 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from aiogram import Bot, Dispatcher, executor, types
 import logging
 
-updater = Updater(token='1189322913:AAGBfHBa_kJCvZngbUwSvRTTgsi39Nnm5pY', use_context=True)
-dispatcher = updater.dispatcher
+logging.basicConfig(level=loggin.INFO)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+bot = Bot(token='1189322913:AAGBfHBa_kJCvZngbUwSvRTTgsi39Nnm5pY')
+dp = Dispatcher
+dispatcher = Dispatcher(bot)
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Where is Febonacci???")      
+@dp.message_handler()
+async def echo(message: types.Message)
+    await message.answer(message.text)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Where is Febonacci???")
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
-updater.start_polling(poll_interval=0.0, timeout=10, clean=False, bootstrap_retries=-1, read_latency=2.0, allowed_updates=None)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
